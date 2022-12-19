ec366bb07e115c6e5c054d884f3cc851b44d9a10
tonasoft <45296558+tonasoft@users.noreply.github.com>
2022-12-19 20:50:42 +0000

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,62 +1,36 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * _atoi - convert a string to an integer
- * @s: string
- * Return: int
+ * _atoi - function converts string to an integer
+ * @s: Pointer parameter
+ * Return: returns an integer value
  */
 int _atoi(char *s)
 {
-	int y = _strlen(s);
+	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
 
-	int i;
-
-	int m = 0;
-
-	char tmp;
-
-	int l;
-
-	int j;
-
-	for (i = 0; i < y; i++)
+	while (*(s + count) != '\0')
 	{
-		for (j = '0'; j <= '9'; j++)
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			k *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			if (s[i] != j)
-			{
-				*(s + i) = tmp;
-				m++;
-			}
-			else if (s[i] == j && s[i - 1] == '-')
-				continue;
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
+		count++;
 	}
-	if (m == y - 1)
-		return (0);
-	l = (s - '0');
-	return (l);
-}
-#include "main.h"
-/**
- * _strlen - return the length of a string
- *
- * @s: pointer to string
- *
- * Return: int
- */
-int _strlen(char *s)
-{
-	int i = 1;
 
-	int result = 0;
-
-	char r = *s;
-
-	while
-		(r != '\0') {
-		result++;
-		r = *(s + i);
-		i++;
-		}
-	return (result);
+	for (i = count - size; i < count; i++)
+	{
+		j = j + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (j * k);
 }
